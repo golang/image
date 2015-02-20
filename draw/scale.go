999c3975a8c4776b591e050136e5499f11f4d194
Nigel Tao <nigeltao@golang.org>
2015-02-20 18:28:54 +1100

    draw: autogenerate impl.go.
    Adding fast paths for concrete dst and src image types will be follow-up
    changes.
    
    Change-Id: Ib23e3cc46d9c7a52ebd5eddfbad09177b0b421af
    Reviewed-on: https://go-review.googlesource.com/5417
    Reviewed-by: Rob Pike <r@golang.org>

--- draw/scale.go
+++ draw/scale.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:generate go run gen.go
+
 package draw
 
 // TODO: should Scale and NewScaler also take an Op argument?
