81315f0c68fbff1933c96bdfdb09df67e712f779
Brad Fitzpatrick <bradfitz@golang.org>
2016-07-15 14:54:48 +0000

    webp: mark package as requiring Go 1.6 for NYCbCrA
    See https://build.golang.org/log/1e0572f27630c3a896f17722d89b6cc35d9f52d8
    
    Updates golang/go#11811
    
    Change-Id: I0db67f2c8586b1c36608c6131c803eb26fb87751
    Reviewed-on: https://go-review.googlesource.com/24945
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- webp/decode.go
+++ webp/decode.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build go1.6
+
 // Package webp implements a decoder for WEBP images.
 //
 // WEBP is defined at:
